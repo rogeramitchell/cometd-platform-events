b3da57f55c84094ee008a3c246387dbe256ee01a
Roger Mitchell <rogeramitchell@gmail.com>
2017-06-29 11:31:12 -0700

    annotating changes to app.js

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-let baseUrl = 'https://rogermitchell-dev-ed.my.salesforce.com/cometd/40.0'; // place your base instance URL here
-let token = ''; // place your token here
+/*
+	set this as your instance url appended with /cometd/40.0
+	example: https://na10.salesforce.com/cometd/40.0
+*/
+let baseUrl = 'https://rogermitchell-dev-ed.my.salesforce.com/cometd/40.0';
 
+/*
+	either make an OAuth call and retrieve the token,
+	or copy this value from the `sid` cookie associated
+	with your baseUrl from within the browser
+*/
+let token = '00D1949343kklakfdsfs!iafdsf;lasdf030iqr23jqllkfklsdflsdkf';
+
+/*
+	set this as the API name of your platform event
+*/
+let platformEventName = 'User_Update__e';
+
+// define node modules
 require('cometd-nodejs-client').adapt();
-
 let lib = require('cometd');
 let cometd = new lib.CometD();
 
+// configure this instance of CometD
 cometd.configure({
 	url: baseUrl,
 	requestHeaders: { 
@@ -14,10 +30,11 @@ cometd.configure({
 	appendMessageTypeToURL: false
 });
 
+// handle the handshake's success
 cometd.handshake((shake) => {
 	if(shake.successful) {
 		// set your event here
-		cometd.subscribe('/event/User_Update__e', (message) => {
+		cometd.subscribe('/event/' + platformEventName, (message) => {
 			console.log(message.data);
 		});
 	}
